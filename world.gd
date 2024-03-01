7b8b5ac46d36b510e4de4b4dc0b5e7a407191dbf
Zack MacKeil <zackmackeil@pop-os.localdomain>
2024-02-29 22:02:22 -0500

    basic additions of logic

--- world.gd
+++ world.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
+enum GameStates {Start, BuildUp1, Buildup2, Bilp1, BuildUp3, Bilp2}
+
+var State: GameStates = GameStates.Start
 var paused: bool = false
+var playerCanLock: bool = false
+@onready var Objective: Label = $ObjectiveContainer/Objective
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	pass # Replace with function body.
+	if (State == GameStates.Start):
+		Objective.text = "Objective: Lock the front door"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +25,28 @@ func _input(event):
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			
+	if(event.is_action_pressed("interact")):
+		if(State == GameStates.Start and playerCanLock):
+			#"Lock" the front door
+			pass
+
+
+func on_player_canReach_FrontDoor(body):
+	if(State == GameStates.Start):
+		#continue logic
+		if(body is Player):
+			playerCanLock = true
+	pass # Replace with function body.
+
+
+func on_player_cantReach_FrontDoor(body):
+	if(body is Player):
+		playerCanLock = false
+	pass # Replace with function body.
+
+
+func _on_door_lock_sound_finished():
+	State = GameStates.BuildUp1
+	#crap now I need to make the TV and start all that stuff
+	pass # Replace with function body.
